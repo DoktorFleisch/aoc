3a95150e64d74e50373f0bf3a6137502d5a5b9ce
Daniel Warkus <daniel.warkus@hhu.de>
2024-12-03 23:02:26 +0100

    day 2 prep

---
+++ Day 2/data_calc.py
@@ -0,0 +1,13 @@
+def load_data():
+    reports_list = []
+
+    with open("day_2_input.txt", "r") as f:
+                data = f.readlines()
+
+
+    for line in data:
+        reports_list.append
+
+    return data
+
+
