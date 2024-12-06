cfb8c0bf2314bd49e5f775d06d2e24789a220a3c
DoktorFleisch <44265452+DoktorFleisch@users.noreply.github.com>
2024-12-06 18:32:42 +0100

    puzzle 1 solved, puzzle 2 bug

--- Day 2/data_calc.py
+++ Day 2/data_calc.py
@@ -2,12 +2,42 @@ def load_data():
     reports_list = []
 
     with open("day_2_input.txt", "r") as f:
-                data = f.readlines()
+        data = f.readlines()
 
+    return [list(map(int, line.split())) for line in data]
+
+
+def check_safety(data_list):
+    increased = all([data_list[i] < data_list[i + 1] for i in range(len(data_list) - 1)])
+    decreased = all([data_list[i] > data_list[i + 1] for i in range(len(data_list) - 1)])
+
+    if not (increased or decreased):
+        return False
+
+    differences = [abs(data_list[i] - data_list[i + 1]) for i in range(len(data_list) - 1)]
+
+    return all(1 <= diff <= 3 for diff in differences)
+
+
+def count_safe_combinations(data):
+    return sum([check_safety(line) for line in data])
+
+
+def can_be_made_safe(data_list):
+    for i in range(len(data_list)-1):
+        modified_data = data_list[:i] + data_list[i + 1:]
+
+        if check_safety(modified_data):
+            return True
+
+    return False
+
+
+def count_safe_level_combinations(data):
+    sum_safe = 0
 
     for line in data:
-        reports_list.append
+        if check_safety(line) or can_be_made_safe(line):
+            sum_safe += 1
 
-    return data
-
-
+    return sum_safe
